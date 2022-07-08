86bfeff666c732d810d08785855519dad9abeff4
GitHub Action <action@github.com>
2022-07-08 14:31:32 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "unlikely-intellij-zf1-plugin"
---
+++ src/main/kotlin/com/github/njd/unlikelyintellijzf1plugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.njd.unlikelyintellijzf1plugin.services
+
+import com.github.njd.unlikelyintellijzf1plugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
